f01e7de0eb9d0cc10ece1ae61e2338b9409c813b
guilherme <guilhermephellippe@outlook.com>
2023-10-31 12:06:55 -0300

    change url api

--- src/api/url-api.ts
+++ src/api/url-api.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:3434"
-// export const API_URL = "https://wip.wipzee.com/"
+// export const API_URL = "http://localhost:3434"
+export const API_URL = "https://wip.wipzee.com/"
 
