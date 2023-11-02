9effe7ff19f853a81a4ee102a576aa0b98ba6372
guilherme <guilhermephellippe@outlook.com>
2023-11-02 12:56:00 -0300

    add dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // Configuração específica para produção
-  build: {
-    // Define o diretório de saída para os arquivos de build
-    outDir: 'dist',
-
-    // Gere arquivos sourcemap (opcional)
-    sourcemap: false, // Defina como true para gerar sourcemaps
-
-    // Habilita o uso de Content Delivery Networks (CDNs) para dependências comuns
-    rollupOptions: {
-      external: ['node_modules/@phosphor-icons/**'],
-    },
-  },
 })
