225e5dedba965019878af3e64ef82b5bbd808366
guilherme <guilhermephellippe@outlook.com>
2023-11-29 12:26:56 -0300

    add acess on paymnets

--- src/pages/thanks/Thanks.tsx
+++ src/pages/thanks/Thanks.tsx
@@ -6,7 +6,7 @@ export default function Thanks() {
         <div className="w-screen h-screen bg-zinc-700 grid place-items-center">
             <div className="">
                 <h2>Obrigado por assinar nossa plataforma</h2>
-                <ButtonMain onClick={()=> window.close()}>Acessar plataforma</ButtonMain>
+                <ButtonMain onClick={()=> window.open("https://wipzee.com/panel")}>Acessar plataforma</ButtonMain>
             </div>
         </div>
     )
