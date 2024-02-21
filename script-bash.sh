29b91303650e6d628dc09750882609dd1a446592
guilherme <guilhermephellippe@outlook.com>
2024-02-21 10:38:46 -0300

    'Atualizações'

--- script-bash.sh
+++ script-bash.sh
@@ -2,5 +2,5 @@
 commit_msg="${npm_config_msg:-'Atualizações'}"
 git commit -m "$commit_msg"
 shift # Isso remove o primeiro argumento ("mensagem de commit") da lista de argumentos
-git push origin main "$@"
+git push origin main
 
