c85f42c4243d6b557f7cf49ebb118dffadac6ea2
guilherme <guilhermephellippe@outlook.com>
2024-02-21 10:47:11 -0300

    chatSchema call to action is optional

---
+++ interface_build/assets/Panel-cSYHaKl0.js
@@ -0,0 +1,7 @@
+import{r as e,_ as n,C as r,u as c,a as d,j as t,I as u}from"./index-u6HZ030z.js";const x=e.lazy(()=>n(()=>import("./MainPanelClient-1YshfFbE.js"),__vite__mapDeps([0,1,2,3]))),_=e.lazy(()=>n(()=>import("./NavigatePanelClient-njQJTxuo.js"),__vite__mapDeps([4,1,2,3])));function g(){const{setClient:i,client:o}=e.useContext(r),l=c();return e.useEffect(()=>{(async()=>{const a=localStorage.getItem("token"),s=a&&await d(a);s?i(s.data.client):l("/login")})()},[]),t.jsx(e.Suspense,{fallback:t.jsx(u,{}),children:o&&t.jsxs("div",{className:"w-screen h-screen overflow-hidden flex justify-start items-start  text-dark dark:text-light",children:[t.jsx(_,{}),t.jsx(x,{})]})})}export{g as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/MainPanelClient-1YshfFbE.js","assets/index-u6HZ030z.js","assets/index-jc7XKbZ7.css","assets/messagesEventManager-09AmoLjj.js","assets/NavigatePanelClient-njQJTxuo.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
