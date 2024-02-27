3a9aea7474adfeb70a3b1fbb84aaadac54b5cc92
guilherme <guilhermephellippe@outlook.com>
2024-02-27 15:57:45 -0300

    debug upload image 2.0

--- interface_build/assets/Panel-yeteK9Yv.js
+++
@@ -1,7 +0,0 @@
-import{r as e,_ as n,C as r,u as c,a as d,j as t,I as u}from"./index-vlIuE4hZ.js";const x=e.lazy(()=>n(()=>import("./MainPanelClient--VebCGJJ.js"),__vite__mapDeps([0,1,2,3]))),_=e.lazy(()=>n(()=>import("./NavigatePanelClient-_T7iUnil.js"),__vite__mapDeps([4,1,2,3])));function g(){const{setClient:i,client:o}=e.useContext(r),l=c();return e.useEffect(()=>{(async()=>{const a=localStorage.getItem("token"),s=a&&await d(a);s?i(s.data.client):l("/login")})()},[]),t.jsx(e.Suspense,{fallback:t.jsx(u,{}),children:o&&t.jsxs("div",{className:"w-screen h-screen overflow-hidden flex justify-start items-start  text-dark dark:text-light",children:[t.jsx(_,{}),t.jsx(x,{})]})})}export{g as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/MainPanelClient--VebCGJJ.js","assets/index-vlIuE4hZ.js","assets/index-3xEE7pP_.css","assets/messagesEventManager-Ambd-xXe.js","assets/NavigatePanelClient-_T7iUnil.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
--- src/api/uploadImages.ts
+++ src/api/uploadImages.ts
@@ -2,13 +2,11 @@ import axios from "axios";
 import { API_URL } from "./url-api";
 
 export async function uploadImage(file: any) {
-    console.log(file)
     try {
-        console.log(file.size < 4000000)
+        console.log("This image has:", Number(file.size / (1024 * 1024)))
         if (file.size < 4000000) {
             const reader = new FileReader();
             reader.readAsDataURL(file);
-            console.log(reader)
 
             const form = new FormData();
             form.append('image', file);
