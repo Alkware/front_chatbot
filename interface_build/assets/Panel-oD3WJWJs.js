f5ad0ee66e46c6e078241899b333549355434141
guilherme <guilhermephellippe@outlook.com>
2024-03-28 17:26:58 -0300

    hhome v3

---
+++ interface_build/assets/Panel-oD3WJWJs.js
@@ -0,0 +1,7 @@
+import{r as e,_ as n,C as r,u as c,a as d,j as t,I as u}from"./index-d94RzxmI.js";const x=e.lazy(()=>n(()=>import("./MainPanelClient-f33CAVER.js"),__vite__mapDeps([0,1,2,3]))),_=e.lazy(()=>n(()=>import("./NavigatePanelClient-bXW2JGvd.js"),__vite__mapDeps([4,1,2,3])));function g(){const{setClient:i,client:o}=e.useContext(r),l=c();return e.useEffect(()=>{(async()=>{const a=localStorage.getItem("token"),s=a&&await d(a);s?i(s.data.client):l("/login")})()},[]),t.jsx(e.Suspense,{fallback:t.jsx(u,{}),children:o&&t.jsxs("div",{className:"w-screen h-screen overflow-hidden flex justify-start items-start text-dark dark:text-light",children:[t.jsx(_,{}),t.jsx(x,{})]})})}export{g as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/MainPanelClient-f33CAVER.js","assets/index-d94RzxmI.js","assets/index-feVfzoft.css","assets/messagesEventManager-GFVxQ5tl.js","assets/NavigatePanelClient-bXW2JGvd.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
--- src/pages/Home/components/Main/components/Presentation/Presentation.tsx
+++ src/pages/Home/components/Main/components/Presentation/Presentation.tsx
@@ -1,36 +1,70 @@
+import imgRobo from "../../../../../../assests/robo-home.svg"
+import { Button } from "../../../../../../components/button/Button"
+import { FaStar } from "react-icons/fa6"
+
+
 export function Presentation() {
     return (
         <section
             id="home"
             className="w-screen h-screen flex flex-col items-center justify-center"
         >
-            <div className="w-full px-4 max-w-[1300px] flex gap-4">
-                <div className="w-1/2 flex flex-col gap-4 items-center z-10">
+            <div className="w-full px-4 max-w-[1300px] flex gap-4 items-center">
 
-                    <h1 className="w-full text-5xl md:text-3xl font-bold uppercase font-sans animate-jump-screen">
+                <div className="w-1/2 flex flex-col gap-4 items-center px-4 z-10">
+                    <h1 className="w-full text-4xl lg:text-5xl font-bold uppercase font-sans animate-jump-screen">
                         Transforme seu <br />atendimento
                     </h1>
                     <div className="w-full flex gap-2 items-center">
                         <h2
-                            className="text-3xl md:text-2xl"
+                            className="text-2xl xl:text-3xl"
                         >
                             Venda mais com um
                         </h2>
-                        <span className="text-3xl md:text-2xl bg-red-500 font-bold rounded-md animate-jump-screen">único link inteligente</span>
+                        <span className="text-xl xl:text-3xl lg:text-2xl bg-red-500 font-bold rounded-md animate-jump-screen">único link inteligente</span>
                     </div>
                     <h3
-                        className="text-xl md:text-lg opacity-70 animate-display-screen"
-                    >
-                        Descubra a solução ideal para otimizar suas vendas e reduzir reclamações, de forma simples e eficiente com o uso da inteligênia artificial.</h3>
+                        className="text-lg lg:text-xl opacity-70 animate-display-screen"
+                    >Descubra a solução ideal para otimizar suas vendas e reduzir reclamações, de forma simples e eficiente com o uso da inteligênia artificial.</h3>
 
+                    <Button
+                        customClass="neon-effect w-full rounded-sm text-xl font-bold my-8 uppercase"
+                    >Crie seu link agora</Button>
                 </div>
-                <div className="w-1/2 flex flex-col items-center justify-center pt-12 relative">
+                <div className="w-1/2 flex flex-col items-center justify-center pt-12 relative z-10">
                     <img
-                        src="https://i.ibb.co/RvX8XRX/com-unscreen.gif"
+                        src={imgRobo}
                         alt=""
                         className="w-full h-full cover"
                     />
                 </div>
+            </div>
+            <div className="max-w-[1000px] w-full flex flex-col absolute bottom-[10vw]">
+                {
+                    Array.from({ length: 8 }).map((_, indexContainer) =>
+                        <div
+                            className="w-full flex"
+                            style={{ paddingLeft: `${(indexContainer) * 4}vw`, justifyContent: `${indexContainer < 2 ? "space-between" : indexContainer < 4 ? "space-evenly" : "space-around"}` }}
+                        >
+                            {
+                                Array.from({ length: 4 }).map((_, index) =>
+                                    indexContainer % 2 ?
+                                        <FaStar
+                                            data-animate={index === 0 ? 1 : index}
+                                            className="fill-primary-100 text-sm data-[animate='1']:animate-rising-10 data-[animate='2']:animate-rising-14 data-[animate='3']:animate-rising-18 z-0 opacity-15"
+                                        />
+                                        :
+                                        <FaStar
+                                            data-animate={index === 0 ? 1 : index}
+                                            className="fill-primary-100 text-sm data-[animate='1']:animate-rising-reverse-10 data-[animate='2']:animate-rising-reverse-14 data-[animate='3']:animate-rising-reverse-18 z-0 opacity-15"
+                                            style={{ margin: `0 ${index}px` }}
+                                        />
+
+                                )
+                            }
+                        </div>
+                    )
+                }
             </div>
         </section>
     )
