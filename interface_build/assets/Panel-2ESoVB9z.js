f6a424192321cb89a97cc46b7d8086e4cbb1b9bb
guilherme <guilhermephellippe@outlook.com>
2024-04-25 18:23:15 -0300

    fix plans

--- interface_build/assets/Panel-2ESoVB9z.js
+++
@@ -1,7 +0,0 @@
-import{r as e,_ as n,C as r,u as c,a as u,j as t,I as x}from"./index-LydCxvWI.js";const d=e.lazy(()=>n(()=>import("./MainPanelClient-9uBpxgEO.js"),__vite__mapDeps([0,1,2,3]))),_=e.lazy(()=>n(()=>import("./NavigatePanelClient-QAkLRstq.js"),__vite__mapDeps([4,1,2,3])));function g(){const{setClient:i,client:o}=e.useContext(r),l=c();return e.useEffect(()=>{(async()=>{const a=localStorage.getItem("token"),s=a&&await u(a);s?i(s.data.client):l("/login")})()},[]),t.jsx(e.Suspense,{fallback:t.jsx(x,{}),children:o&&t.jsxs("div",{className:"w-screen h-screen overflow-hidden flex justify-start items-start text-primary-100 dark:text-light",children:[t.jsx(_,{}),t.jsx(d,{})]})})}export{g as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/MainPanelClient-9uBpxgEO.js","assets/index-LydCxvWI.js","assets/index-kvKagIbY.css","assets/messagesEventManager-SVNLGYGr.js","assets/NavigatePanelClient-QAkLRstq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
