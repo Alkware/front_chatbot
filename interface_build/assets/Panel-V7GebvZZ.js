eb748d157ecfa42a3066fe53812ab40e945fd836
guilherme <guilhermephellippe@outlook.com>
2024-05-01 16:43:58 -0300

    fix database form 02

---
+++ interface_build/assets/Panel-V7GebvZZ.js
@@ -0,0 +1,7 @@
+import{r as e,_ as n,C as r,u as c,a as u,j as t,I as x}from"./index-9jEVtDp4.js";const _=e.lazy(()=>n(()=>import("./MainPanelClient-wawb60A7.js"),__vite__mapDeps([0,1,2,3]))),d=e.lazy(()=>n(()=>import("./NavigatePanelClient-cRyB1dSj.js"),__vite__mapDeps([4,1,2,3])));function f(){const{setClient:i,client:l}=e.useContext(r),o=c();return e.useEffect(()=>{(async()=>{const a=localStorage.getItem("token"),s=a&&await u(a);s?i(s.data.client):o("/login")})()},[]),t.jsx(e.Suspense,{fallback:t.jsx(x,{}),children:l&&t.jsxs("div",{className:"w-screen h-screen flex justify-center items-start text-primary-100 dark:text-light",children:[t.jsx(d,{}),t.jsx(_,{})]})})}export{f as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/MainPanelClient-wawb60A7.js","assets/index-9jEVtDp4.js","assets/index-cdbJop7i.css","assets/messagesEventManager-nP9Q9w4t.js","assets/NavigatePanelClient-cRyB1dSj.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
