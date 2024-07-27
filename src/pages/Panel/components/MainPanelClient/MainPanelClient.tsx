c8d6cf344a6bdd3126ec682bdd4f92d92a3e6d39
guilherme <guilhermephellippe@outlook.com>
2024-07-27 14:19:08 -0300

    [fix]: form create database isnt responsive

--- src/pages/Panel/components/MainPanelClient/MainPanelClient.tsx
+++ src/pages/Panel/components/MainPanelClient/MainPanelClient.tsx
@@ -1,6 +1,5 @@
 import PanelHeader from "./components/PanelHeader/PanelHeader";
 import { ControlComponentsSelectedMenu } from "./components/ControlComponentsSelectedMenu/ControlComponentsSelectedMenu";
-import { HelperChat } from "./components/HelperChat/HelperChat";
 
 export default function MainPanelClient() {
 
@@ -8,7 +7,7 @@ export default function MainPanelClient() {
         <div className="w-full min-h-screen md:w-[90%] flex-grow flex flex-col items-center bg-gradient-to-br from-primary-100 from-[10%] to-light to-[20%] dark:to-dark">
             <PanelHeader />
             <ControlComponentsSelectedMenu />
-            <HelperChat />
+            {/* <HelperChat /> */}
         </div>
     )
 };
